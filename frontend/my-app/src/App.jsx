c2d04afe9f8ee776a8544149a45e6c8b4eafe93a
shahmeet199722 <shahmeet199722@hotmail.com>
2025-08-23 20:15:33 +0530

    Basic routing added

--- frontend/my-app/src/App.jsx
+++ frontend/my-app/src/App.jsx
@@ -1,10 +1,34 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 import "./App.css";
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
 
-function App() {
-  return <h1>Hello World</h1>;
-}
+const App = () => {
+  return (
+    <Router>
+      {/*Switch will tell react that if any exact route matches, then no
+      need to check other routes and not route to the redirect code line
+      */}
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+
+        {/*Redirect any unknown routes to the home page */}
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
