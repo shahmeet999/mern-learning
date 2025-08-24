25c0430da77ee1a70e339929ebcd2914f1617471
shahmeet199722 <shahmeet199722@hotmail.com>
2025-08-24 16:29:13 +0530

    48 added

--- frontend/my-app/src/user/components/Navigation/MainNavigation.jsx
+++ frontend/my-app/src/user/components/Navigation/MainNavigation.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./MainNavigation.css";
 import MainHeader from "./MainHeader";
 import { Link } from "react-router-dom";
+import NavLinks from "./NavLinks";
 
 const MainNavigation = (props) => {
   return (
@@ -14,7 +15,9 @@ const MainNavigation = (props) => {
       <h1 className="main-navigation__title">
         <Link to="/">YourPlaces</Link>
       </h1>
-      <nav className="main-navigation__header-nav"></nav>
+      <nav className="main-navigation__header-nav">
+        <NavLinks />
+      </nav>
     </MainHeader>
   );
 };
