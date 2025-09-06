ff2154401ee8b0ba15cc84d66d7f9688e0908b1f
shahmeet199722 <shahmeet199722@hotmail.com>
2025-09-06 13:52:16 +0530

    input added

--- frontend/my-app/src/places/pages/NewPlace.jsx
+++ frontend/my-app/src/places/pages/NewPlace.jsx
@@ -5,7 +5,15 @@ import Input from "../../shared/components/FormElements/Input";
 const NewPlace = () => {
   return (
     <form className="place-form">
-      <Input id="title" element="input" type="text" label="Title" />
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[]}
+        errorText="Please enter a valid title."
+      />
+      
     </form>
   );
 };
