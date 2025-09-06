4067f601de5b677711f62dfce0e0509ced4ecc0f
shahmeet199722 <shahmeet199722@hotmail.com>
2025-09-06 18:10:14 +0530

    Refactor UpdatePlace component; improve form state management and update dummy data

--- frontend/my-app/src/places/pages/UpdatePlace.jsx
+++ frontend/my-app/src/places/pages/UpdatePlace.jsx
@@ -1,49 +1,65 @@
-import React, { use } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
 import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
 import {
   VALIDATOR_REQUIRE,
   VALIDATOR_MINLENGTH,
 } from "../../shared/utils/validators";
-import Button from "../../shared/components/FormElements/Button";
-import "./PlaceForm.css";
 import { useForm } from "../../shared/hooks/form-hook";
+import "./PlaceForm.css";
 
-const dummyPlaces = [
+const DUMMY_PLACES = [
   {
     id: "p1",
     title: "Empire State Building",
     description: "One of the most famous sky scrapers in the world!",
     imageUrl:
-      "https://www.esbnyc.com/sites/default/files/2025-03/ESB-DarkBlueSky.webp",
-    address: "20 W 34th St, New York, NY 10001, United States",
-    location: { lat: 40.7484405, lng: -73.9878584 },
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
     creator: "u1",
   },
   {
     id: "p2",
-    title: "Statue of Liberty",
-    description: "Famous monument in New York City",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
     imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZLYxUBSyZZY66XUnjDDirUB7bUt4tlOA20A&s",
-    address: "Liberty Island, New York, NY 10004, United States",
-    location: { lat: 40.6892494, lng: -74.0445004 },
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
     creator: "u2",
   },
 ];
 
 const UpdatePlace = () => {
   const placeId = useParams().placeId;
-  const identifiedPlace = dummyPlaces.find((p) => p.id === placeId);
+  const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
 
   const [formState, inputHandler] = useForm(
     {
-      title: { value: identifiedPlace.title, isValid: false },
-      description: { value: identifiedPlace.description, isValid: false },
-      address: { value: identifiedPlace.address, isValid: false },
+      title: {
+        value: identifiedPlace.title,
+        isValid: true,
+      },
+      description: {
+        value: identifiedPlace.description,
+        isValid: true,
+      },
     },
-    false
+    true
   );
+
+  const placeUpdateSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(formState.inputs);
+  };
 
   if (!identifiedPlace) {
     return (
@@ -54,7 +70,7 @@ const UpdatePlace = () => {
   }
 
   return (
-    <form className="place-form">
+    <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
       <Input
         id="title"
         element="input"
@@ -66,7 +82,6 @@ const UpdatePlace = () => {
         initialValue={formState.inputs.title.value}
         initialValid={formState.inputs.title.isValid}
       />
-
       <Input
         id="description"
         element="textarea"
@@ -77,7 +92,6 @@ const UpdatePlace = () => {
         initialValue={formState.inputs.description.value}
         initialValid={formState.inputs.description.isValid}
       />
-
       <Button type="submit" disabled={!formState.isValid}>
         UPDATE PLACE
       </Button>
