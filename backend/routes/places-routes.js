f9e182adf9c2ee80488d444a239a18cedacc3e62
shahmeet199722 <shahmeet199722@hotmail.com>
2025-09-13 13:46:22 +0530

    Implement places routes with GET endpoints for place by ID and user ID

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -2,8 +2,44 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  res.json({ message: "Places route is working!" });
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous skyscrapers in the world!",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    address: "20 W 34th St, New York, NY 10001",
+    creator: "u1",
+  },
+];
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid; // { pid: 'p1' }
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+  if (!place) {
+    return res
+      .status(404)
+      .json({ message: "Could not find a place for the provided id." });
+  }
+  res.json({ places: place });
+});
+
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === userId;
+  });
+  if (!place) {
+    return res
+      .status(404)
+      .json({ message: "Could not find a place for the provided user id." });
+  }
+  res.json({ place });
 });
 
 module.exports = router;
