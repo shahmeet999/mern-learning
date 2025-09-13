5e3ff1b1b863b78b4ef6c550dc197576b384ef38
shahmeet199722 <shahmeet199722@hotmail.com>
2025-09-13 14:27:07 +0530

    Implement places controller with GET endpoints for place by ID and user ID

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,49 +1,9 @@
 const express = require("express");
-const HttpError = require("../models/http-error");
 const router = express.Router();
+const placesController = require("../controllers/places-controller");
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous skyscrapers in the world!",
-    location: {
-      lat: 40.7484405,
-      lng: -73.9878584,
-    },
-    address: "20 W 34th St, New York, NY 10001",
-    creator: "u1",
-  },
-];
+router.get("/:pid", placesController.getPlaceById);
 
-router.get("/:pid", (req, res, next) => {
-  const placeId = req.params.pid; // { pid: 'p1' }
-  const place = DUMMY_PLACES.find((p) => {
-    return p.id === placeId;
-  });
-  if (!place) {
-    const error = new HttpError(
-      "Could not find a place for the provided id.",
-      404
-    );
-    return next(error);
-  }
-  res.json({ places: place });
-});
-
-router.get("/user/:uid", (req, res, next) => {
-  const userId = req.params.uid;
-  const place = DUMMY_PLACES.find((p) => {
-    return p.creator === userId;
-  });
-  if (!place) {
-    const error = new HttpError(
-      "Could not find a place for the provided user id.",
-      404
-    );
-    return next(error);
-  }
-  res.json({ place });
-});
+router.get("/user/:uid", placesController.getPlacesByUserId);
 
 module.exports = router;
